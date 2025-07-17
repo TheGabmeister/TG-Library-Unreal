01afa666cd8a4e575d450337c1a7a03d6ade2dad
TheGabmeister <thegabmeister@protonmail.com>
2025-07-17 18:42:59 +0800

    Add Simple Event System

---
+++ SimpleEventSystem/Source/SimpleEventSystem/Private/SimpleEventSystemBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimpleEventSystemBPLibrary.h"
+#include "SimpleEventSystem.h"
+
+USimpleEventSystemBPLibrary::USimpleEventSystemBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float USimpleEventSystemBPLibrary::SimpleEventSystemSampleFunction(float Param)
+{
+	return -1;
+}
+
